b5f803c7e9a56c36c287162d4c5315b5948900b0
houssam72 <brothersmarket72@gmail.com>
2024-01-11 12:06:53 +0100

    Add Authorization , Protect routes ,Current User and more

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -7,6 +7,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import { Query } from 'express-serve-static-core';
 import * as mongoose from 'mongoose';
+import { User } from '../auth/schemas/user.schema';
 
 @Injectable()
 export class BookService {
@@ -35,8 +36,9 @@ export class BookService {
     return books;
   }
 
-  async create(book: Book): Promise<Book> {
-    const res = await this.bookModel.create(book);
+  async create(book: Book, user: User): Promise<Book> {
+    const data = Object.assign(book, { user: user._id });
+    const res = await this.bookModel.create(data);
     return res;
   }
 
