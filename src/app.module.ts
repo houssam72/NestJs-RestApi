3fbed2659d2a5fe6d5d9535910dedf0e06e36781
houssam72 <brothersmarket72@gmail.com>
2024-03-11 03:15:56 +0100

    add basic config for my e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import mongoose from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,17 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  beforeAll(async () => {
+    const db = await mongoose.connect(process.env.DB_URI);
+    await db.connection.db.dropDatabase();
+  });
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/book').expect(200).expect([]);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await app.close();
   });
 });
